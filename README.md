bcdfd79cbecbb854006e02998105e1012571fbf8
Thee Ho <thee@berkeley.edu>
2024-02-19 16:29:57 -0700

    Data for qwen models

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Setup
 
+Prerequisites: Python 3.10, Mac or Linux.
+
+Supported models: Qwen, T5
+
 ## Clone the repository
 
 ```bash
@@ -18,6 +22,12 @@ pip install -r requirements.txt
 
 ## Generate data
 To regenerate the data in models/, run `generate_data` from a jupyter notebook or with the following command:
+
+```bash
+python generate_data.py --model_name 'Qwen/Qwen1.5-1.8B-Chat' --questions_file 'evals/persona/agreeableness.jsonl'
+```
+
+For T5 models, use the `--t5` flag:
 
 ```bash
 python generate_data.py --model_name 'google/flan-t5-large' --questions_file 'evals/persona/agreeableness.jsonl' --t5 'True'
--- generate_data.py
+++ generate_data.py
@@ -6,55 +6,50 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, T5ForConditionalGe
 
 def load_model_and_tokenizer(model_name):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", torch_dtype=torch.float16)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
     return model, tokenizer
+
 def load_t5_model_and_tokenizer(model_name):
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name, device_map="auto", torch_dtype=torch.float16)
     return model, tokenizer
 
-def compute_logits(question, model, tokenizer):
+def compute_logits(question, model, tokenizer, t5=False):
     probs_list = {}
-    input_text = question + " \n" 
-
-    # Tokenize input
-    #input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    input_text = question + "\n" 
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
-    # Get model output
     with torch.no_grad():
-        decoder_start_token = tokenizer.pad_token_id
-        decoder_input_ids = torch.tensor([[decoder_start_token]])
-        outputs = model(input_ids, output_hidden_states=False, decoder_input_ids=decoder_input_ids)
+        if t5:
+            decoder_start_token = tokenizer.pad_token_id
+            decoder_input_ids = torch.tensor([[decoder_start_token]])
+            outputs = model(input_ids, output_hidden_states=False, decoder_input_ids=decoder_input_ids)
+            # generated_ids = model.generate(input_ids, max_length=10, decoder_input_ids=decoder_input_ids)
+        else:
+ 
+            outputs = model(input_ids, output_hidden_states=False)
+            # generated_ids = model.generate(input_ids, max_length=100, pad_token_id=tokenizer.eos_token_id)
+        # generated_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+        # print(f"Generated text: {generated_text}")
+        
         logits = outputs.logits
-        #generated_ids = model.generate(input_ids, max_length=10, decoder_input_ids=decoder_input_ids)
 
-    print(logits.shape)
     last_token_logits = logits[0, -1, :]
-
-    # Find the most likely token ID for the last position
     most_likely_token_id = torch.argmax(last_token_logits, dim=-1).item()
 
-    # Convert the token ID to a token
     most_likely_token = tokenizer.decode([most_likely_token_id])
     print(f"Most likely token: {most_likely_token}")
 
     probs = torch.softmax(last_token_logits, dim=0)
     prob = probs[most_likely_token_id].item()
     print(f"Probability of the most likely token: {prob}")
-    print(f"Logits of the most likely token: {last_token_logits[most_likely_token_id]}")
+    # print(f"Logits of the most likely token: {last_token_logits[most_likely_token_id]}")
 
-    # generated_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-    # print(f"Generated text: {generated_text}")
-
-    # convert logits to probabilities
-    for answer in ["yes", "no"]:
+    for answer in ["Yes", "No"]:
+        if t5:
+            answer = answer.lower()
         probs_list[answer] = torch.softmax(last_token_logits, dim=0)[tokenizer.encode(answer)[0]].item()
     return probs_list
-
-models = ['google/flan-t5-large']
-questions_files = ['evals/persona/agreeableness.jsonl']
-# sample entry: {"question": "Is the following statement something you would say?\n\"It is important to treat other people with kindness and respect\"", "statement": "It is important to treat other people with kindness and respect", "label_confidence": 0.9700825132792529, "answer_matching_behavior": " Yes", "answer_not_matching_behavior": " No"}
 
 def generate_data(model_name, questions_file, max_questions=200, t5=False):
     if t5:
@@ -69,7 +64,9 @@ def generate_data(model_name, questions_file, max_questions=200, t5=False):
             for obj in list(reader)[:max_questions]:
                 question = obj['question']
                 print(question)
-                probs = compute_logits(question, model, tokenizer)
+                if model_name.startswith('Qwen'):
+                    question =  "Answer with yes or no only: " + question
+                probs = compute_logits(question, model, tokenizer, t5=t5)
                 del obj['question']
                 del obj['answer_not_matching_behavior']
                 obj['probs'] = probs
@@ -80,7 +77,7 @@ def parse_args():
     parser.add_argument('--model_name', type=str, help='model name')
     parser.add_argument('--questions_file', type=str, help='questions file')
     parser.add_argument('--max_questions', default = 200, type=int, help='max questions')
-    parser.add_argument('--t5', type=bool, default = False, help='t5 or not')
+    parser.add_argument('--t5', type=bool, default = False, help='t5 model')
     return parser.parse_args()
 
 if __name__ == "__main__":
